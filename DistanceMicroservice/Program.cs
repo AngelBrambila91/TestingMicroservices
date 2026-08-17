d2abcfc1bcb2f8bb1bb46c79623225d675d459e2
AngelBrambila91 <malthael.91@gmail.com>
2026-08-17 16:31:09 -0600

    Added Swagger for testcases usage, clean local secret and renamed apinames

--- DistanceMicroservice/Program.cs
+++ DistanceMicroservice/Program.cs
@@ -2,6 +2,7 @@ using GoogleRouteService;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<GoogleRouteServices>();
 
 var app = builder.Build();
@@ -11,19 +12,23 @@ IConfiguration config = app.Configuration;
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GoogleRouteService v1"));
 }
 
 app.UseHttpsRedirection();
 app.MapPost("/getdistanceinfo", (Addresses addresses, GoogleRouteServices googleRouteService) =>
-{
-    var apiUrl = config["googleRoutesApi:apiUrl"]
-                  ?? throw new InvalidOperationException("URL key, googleRouteApiUrl, not found.");
-    var apiKey = config["googleRoutesApi:apiKey"]
-                  ?? throw new InvalidOperationException("API key, googleRouteApiKey, not found in user secrets.");
-
-
-    var response = googleRouteService.GetRouteInfo(addresses, apiUrl, apiKey);
-    return response;
-})
-.WithName("GetDistanceInfo");
-app.Run();
+              {
+              var apiUrl = config["googleRoutesApi:apiUrl"]
+              ?? throw new InvalidOperationException("URL key, googleRouteApiUrl, not found.");
+              var apiKey = config["googleRoutesApi:apiKey"]
+              ?? throw new InvalidOperationException("API key, googleRouteApiKey, not found in user secrets.");
+            
+            
+              var response = googleRouteService.GetRouteInfo(addresses, apiUrl, apiKey);
+              return response;
+              })
+              .WithName("GetDistanceInfo");
+            
+            
+              app.Run();
